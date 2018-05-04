771beffca148ff1774f4e6259911ce90ed365643
2018mcrotty <mjc2011@verizon.net>
2018-05-04 14:00:05 -0400

    test commit

--- app/src/main/java/edu/tjhsst/b001e0/MainActivity.java
+++ app/src/main/java/edu/tjhsst/b001e0/MainActivity.java
@@ -1,14 +1,26 @@
 package edu.tjhsst.b001e0;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button mStart;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int test = 16;
+        mStart = (Button)findViewById(R.id.start_button);
+
+        mStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent startIntent = new Intent(MainActivity.this, GameSetUp.class);
+                startActivity(startIntent);
+            }
+        });
     }
 }
